ac93e8ed39912b3c71aea35d537d50034f16cc8a
kate <iurchenko.kateryna@gmail.com>
2019-11-26 22:44:40 +0200

    added 2 negative tests (by DataProvider's data)

--- src/main/java/com/hometask/Constants.java
+++ src/main/java/com/hometask/Constants.java
@@ -5,7 +5,7 @@ package com.hometask;
  */
 public class Constants {
     public static final String TEST_USER_ACCESS_TOKEN = "EAAMZB9ooLWKYBAGiUVwCNZCxNOkEEIG5RiQ9rs8yfJHZAiGP9N5DVAETZADGofZB3Dwnpu6oMv5ulyZCeE7UgAIi6ivkr0vP8FZAHJ02UF7ju7HtKXc1DEquv8EWA1NOsdCWPZBvAjje0rB4XZCUX9wyB9SuzUDvZCZAxpKobUbWdySopZAlE8UVk6Lve50nA6mXEZAJqfXIQ3JLuZCwZDZD";
-    public static final String PAGE_ACCESS_TOKEN = "EAAMZB9ooLWKYBAOGSksAzJ2w6oVIEYo9aJFZC01FM070rKLGI3I3OVNsOfCK9p6optALrPgR7tqJZCCI4IZBszhw1QfP9vkfOgFiGVJ8PY0b4y2B0X1HCG23OyBkAMHObaS51gCxvCsYMrgTqomzlYbQkzM5kj8yOqllZCU9ZBpvJyW1pbMWfZBom41mfvb510vZCvmGYBrVjaQcK1Y5IW1x";
+    public static final String PAGE_ACCESS_TOKEN = "EAAMZB9ooLWKYBAPnkqdSWLL6AirUdYHDG5Ycy1fHw9iZC3qfqTjZCEmKcvoObuRZCL1LOcPgFpy6LsegUYcVZC4RH7BW6mrosc8oFXWW3RdSR0xw8rV9XDBe3sZC2Av5C03omOOh2ZBlXlR7xDg65UvvLkMc29SriduvZA03PaM0nYhdprDXAfpo96ZAYJ4di7keJnEHVS63uHYIHxqpZBJZC9G";
     public static final String TEST_PAGE_ID = "108472243958960";
     public static final String USER_PASS = "pRv9YFCRqJzw!wS";
     public static final String USER_NAME = "jsfsntwswr_1574609723@tfbnw.net";
--- src/test/java/com/hometask/api/PostTest.java
+++ src/test/java/com/hometask/api/PostTest.java
@@ -5,13 +5,18 @@ import com.restfb.DefaultFacebookClient;
 import com.restfb.FacebookClient;
 import com.restfb.Parameter;
 import com.restfb.Version;
+import com.restfb.exception.FacebookOAuthException;
 import com.restfb.types.FacebookType;
 import com.restfb.types.Post;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.util.List;
+
+import static org.testng.Assert.assertEquals;
 
 /**
  * Created by kate on 11/24/19.
@@ -20,14 +25,20 @@ public class PostTest {
     public static final String PAGE_ENDPOINT = Constants.TEST_PAGE_ID + "/feed";
     FacebookClient client;
 
+    @DataProvider
+    public static Object[][] getTextValuesForNegativeTest() {
+        return new Object[][]{{""}, {RandomStringUtils.randomAlphanumeric(63207)}};
+    }
+
     @BeforeTest
     public void init() {
-//        client = new DefaultFacebookClient(Constants.TEST_USER_ACCESS_TOKEN, Version.VERSION_2_10);
         client = new DefaultFacebookClient(Constants.PAGE_ACCESS_TOKEN, Version.VERSION_2_10);
     }
 
     /**
-     * add also test for publishing photo/video/link/add comment on post
+     * add also test for publishing photo/video/link/add comment on post;
+     * create textual post with: 1 symbol, 63206 symbol, 63207 symbol (negative),
+     * without 'message' parameter(negative), with null value(negative)
      */
     @Test
     public void createPostTest() {
@@ -43,17 +54,27 @@ public class PostTest {
                 .orElseThrow(() -> new AssertionError(String.format("Message with %s was not posted!", postId)));
     }
 
+    @Test(dataProvider = "getTextValuesForNegativeTest")
+    public void createPostNegativeTest(String input) {
+        FacebookOAuthException message = client.publish(PAGE_ENDPOINT,
+                FacebookOAuthException.class,
+                Parameter.with("message", input));
+        assertEquals((int) message.getHttpStatusCode(), 400, "Status code is incorrect");
+        assertEquals(message.getErrorMessage(), "Invalid parameter", "Error message is incorrect");
+    }
+
     private List<Post> getPostsList() {
         return client.fetchConnection(PAGE_ENDPOINT, Post.class).getData();
     }
 
     /**
      * add also test on update different media type content(e.g. photo, video, images, links)in post
+     * add also edit with: remove all text(negative?), all media,
      */
     @Test
     public void updateLastPostTest() {
         List<Post> postsList = getPostsList();
-        Post lastPost = postsList.get(postsList.size() - 1);
+        Post lastPost = postsList.get(0);
         String message = lastPost.getMessage();
 
         client.deleteObject(lastPost.getId());
@@ -64,8 +85,8 @@ public class PostTest {
                 Parameter.with("message", updatedValue));
 
         postsList = getPostsList();
-        lastPost = postsList.get(postsList.size() - 1);
-        Assert.assertEquals(lastPost.getMessage(), updatedValue, "Message was not updated");
+        lastPost = postsList.get(0);
+        assertEquals(lastPost.getMessage(), updatedValue, "Message was not updated");
     }
 
     /**
@@ -74,7 +95,7 @@ public class PostTest {
     @Test
     public void deleteLastPostTest() {
         List<Post> postsList = getPostsList();
-        Post lastPost = postsList.get(postsList.size() - 1);
+        Post lastPost = postsList.get(0);
         boolean isDeleted = client.deleteObject(lastPost.getObjectId());
         Assert.assertTrue(isDeleted, "Post was not deleted");
     }
